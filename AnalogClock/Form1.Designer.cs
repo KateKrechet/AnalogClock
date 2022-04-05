9b4acbf1ed53f2f2d742d4f3ab5f88a38cc8ed42
KateKrechet <k.krechet@yandex.ru>
2022-04-05 20:01:21 +0300

    Add project files.

---
+++ AnalogClock/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace AnalogClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myAnalogClock1 = new AnalogClock.MyAnalogClock();
+            ((System.ComponentModel.ISupportInitialize)(this.myAnalogClock1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myAnalogClock1
+            // 
+            this.myAnalogClock1.BackgroundClockImage = global::AnalogClock.Properties.Resources.clock;
+            this.myAnalogClock1.BackgroundColor = System.Drawing.Color.Aqua;
+            this.myAnalogClock1.HourArrowColor = System.Drawing.Color.Black;
+            this.myAnalogClock1.HourArrowWidth = 6;
+            this.myAnalogClock1.Location = new System.Drawing.Point(148, 89);
+            this.myAnalogClock1.MinuteArrowColor = System.Drawing.Color.Red;
+            this.myAnalogClock1.MinuteArrowWidth = 4;
+            this.myAnalogClock1.Name = "myAnalogClock1";
+            this.myAnalogClock1.SecondArrowColor = System.Drawing.Color.Blue;
+            this.myAnalogClock1.SecondArrowWidth = 2;
+            this.myAnalogClock1.Size = new System.Drawing.Size(250, 250);
+            this.myAnalogClock1.TabIndex = 0;
+            this.myAnalogClock1.TabStop = false;
+            this.myAnalogClock1.UseImage = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myAnalogClock1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myAnalogClock1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyAnalogClock myAnalogClock1;
+    }
+}
+
---
+++ AnalogClock/MyAnalogClock.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AnalogClock
+{
+    class MyAnalogClock : PictureBox
+    {
+        public Color BackgroundColor { get; set; }
+        public Color HourArrowColor { get; set; }
+        public Color MinuteArrowColor { get; set; }
+        public Color SecondArrowColor { get; set; }
+        public int HourArrowWidth { get; set; }
+        public int MinuteArrowWidth { get; set; }
+        public int SecondArrowWidth { get; set; }
+        public Image BackgroundClockImage { get; set; }
+        public bool UseImage { get; set; }//картинка или фон
+        private DateTime time;
+        private Timer timer;
+        private Graphics graphics;
+
+        public MyAnalogClock()
+        {
+            BackgroundColor = Color.Aqua;
+            HourArrowColor = Color.Black;
+            MinuteArrowColor = Color.Red;
+            SecondArrowColor = Color.Blue;
+            HourArrowWidth = 6;
+            MinuteArrowWidth = 4;
+            SecondArrowWidth = 2;
+            Width = 250;
+            Height = 250;
+            UseImage = false;
+            timer = new Timer();
+            timer.Interval = 1000;//1 раз в секунду будет обновляться
+            timer.Tick += TimerTick;
+            graphics = CreateGraphics();
+        }
+        void DrawCircle()
+        {
+            SolidBrush brush = new SolidBrush(BackgroundColor);
+            graphics.FillEllipse(brush, 0, 0, Width, Height);
+        }
+        private void DrawArrow(Color arrowColor, int width,
+            int length, float angle)
+        {
+            angle -= 90;
+            float x = Width / 2;
+            float y = Height / 2;
+
+            float x1 = (float)Math.Cos((Math.PI / 180) * angle) * length + x;
+            float y1 = (float)Math.Sin((Math.PI / 180) * angle) * length + y;
+            Pen pen = new Pen(arrowColor, width);
+            graphics.DrawLine(pen, x, y, x1, y1);
+        }
+        public void Start()
+        {
+            timer.Enabled = true;
+        }
+        public void Stop()
+        {
+            timer.Enabled = false;
+        }
+        void TimerTick(object sender, EventArgs e)
+        {
+            int arrowLength = Width > Height ? Height : Width;//берем по меньшей стороне
+            arrowLength /= 2;
+            graphics = CreateGraphics();
+            if (UseImage == true && BackgroundClockImage != null)
+            {
+                graphics.DrawImage(BackgroundClockImage, 0, 0, Width, Height);
+            }
+            else DrawCircle();
+           
+            time = DateTime.Now;//берем время сейчас
+            DrawArrow(HourArrowColor, HourArrowWidth, (int)(arrowLength * 0.5),
+                time.Hour * 30+time.Minute/2);//30=360/12
+            DrawArrow(MinuteArrowColor, MinuteArrowWidth, (int)(arrowLength * 0.7), time.Minute * 6);
+            DrawArrow(SecondArrowColor, SecondArrowWidth, (int)(arrowLength * 0.9), time.Second * 6);//==360/60
+        }
+    }
+}
